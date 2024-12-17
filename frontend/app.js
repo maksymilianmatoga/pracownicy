d4e517c78e274d699d1264bda088b3415e2dbe6b
Maksymilian Matoga <124835665+maksymilianmatoga@users.noreply.github.com>
2024-12-17 16:13:10 +0100

    Aktualiza app.js

--- frontend/app.js
+++ frontend/app.js
@@ -18,16 +18,13 @@ document
 
     // Wysyłanie danych do backendu
     try {
-      const response = await fetch(
-        "https://pracownicy-raports-projects-47cfe79b.vercel.app/",
-        {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify({ name, lastName }),
-        }
-      );
+      const response = await fetch("https://https://pracownicy.vercel.app/", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ name, lastName }),
+      });
 
       const data = await response.json();
       messageElement.textContent = data.message || "Pracownik został dodany!";
