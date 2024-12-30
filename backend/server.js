88c7f086ede0878a8110f8c012c11cbfcf3afd1d
Maksymilian Matoga <124835665+maksymilianmatoga@users.noreply.github.com>
2024-12-30 19:18:18 +0100

    Aktualizacja portów

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,7 @@ const cors = require("cors");
 const bodyParser = require("body-parser");
 
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 3000;
 
 // Middleware do obsługi CORS
 app.use(
